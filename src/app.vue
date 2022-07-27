6a975e56f4020aa2ecc18010890dfbfa4575be7e
董珂珂 <1132761957@qq.com>
2022-07-27 10:17:40 +0800

    fix：修复样式打包不了的问题

--- src/app.vue
+++ src/app.vue
@@ -4,12 +4,18 @@
       <router-link to="/">Go to Home</router-link>
       <router-link to="/about">Go to About</router-link>
     </header>
-    <router-view></router-view>
+    <body class="body">
+      <router-view></router-view>
+    </body>
   </div>
 </template>
 <style scoped>
   .header{ background-color:brown;}
   .header a{ display: block; }
+  .body{
+    background-color:darkgray;
+    min-height: 600px;
+  }
 </style>
 <script lang="ts" setup>
 import { ref } from 'vue'
